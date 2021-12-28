ae2fe753a28f32ab88b6182b0828054b39be1aad
chheangg <balldremz@gmail.com>
2021-12-28 14:56:10 +0700

    Add book removal and status switch function
    Book can be removed by clicking on the minus icon, while the read status
    of the book can be changed.

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ let bookPage = document.querySelector('#page')
 let bookAuthor = document.querySelector('#author')
 let bookGenre = document.querySelector('#genre')
 let bookStatus = document.getElementsByName('status');
+let statusBtn = document.querySelectorAll('.status');
+let removeBtn = document.querySelectorAll('.remove-content');;
+let id = 0;
 
 btn.addEventListener('click', () => {
     form.style.cssText = "display: flex;"
@@ -19,6 +22,9 @@ closebtn.addEventListener('click', () => {
 })
 
 submit.addEventListener('click', () => {
+    if (!bookName.value || !bookAuthor.value || !bookPage.value) {
+        return;
+    }
     let status;
     for (i = 0; i < bookStatus.length; i++) {
         if (bookStatus[i].checked) {
@@ -26,21 +32,64 @@ submit.addEventListener('click', () => {
             break;
         }
     }
-    runInput(bookName.value , bookAuthor.value, bookPage.value, bookGenre.value , status.value)
+    runInput(bookName.value , bookAuthor.value, bookPage.value, bookGenre.value , status.value, id)
     form.style.cssText = "display: none;"
     bookName.value = '';
     bookAuthor.value = '';
     bookPage.value = '';
     bookGenre.value = '';
     bookStatus.value = '';
+    id++;
+    removeBtn.forEach((button) => {
+        button.addEventListener('click', removeContent);
+    })
+    statusBtn.forEach((button) => {
+        button.addEventListener('click', changeStatus);
+    })
 })
 
-function book(name, author, page, genre, status) {
+// Change the status button's content and array
+function changeStatus(button) {
+    if (button.target.classList.contains('not-read')) {
+        myLibrary.forEach((book) => {
+            if ( book.id == button.target.getAttribute('data-key')) {
+                book.status =  "Read"
+            }
+        })
+        let content = document.querySelector(`.status-${button.target.getAttribute('data-key')}`);
+        content.textContent = "Read";
+         button.target.classList.remove('not-read');
+         button.target.classList.add('read');
+    } else {
+        myLibrary.forEach((book) => {
+            if ( book.id == button.target.getAttribute('data-key')) {
+                book.status =  "Not Read"
+            }
+        })
+        let content = document.querySelector(`.status-${button.target.getAttribute('data-key')}`);
+        content.textContent = "Not Read";
+        button.target.classList.remove('read');
+        button.target.classList.add('not-read'); 
+    }
+}
+
+// Removes content from both the array and the page using data-key.
+function removeContent(button) {
+    myLibrary.forEach((book) => {
+        if ( book.id == button.target.getAttribute('data-key')) {
+            container.removeChild(document.querySelector(`span[data-key="${book.id}"]`));
+            myLibrary.splice(myLibrary.indexOf(book), 1);
+        }
+    })
+} 
+
+function book(name, author, page, genre, status, id) {
     this.name = name;
     this.author = author;
     this.page = page;
     this.genre = genre;
     this.status = status;
+    this.id = id;
 }
 
 function addBookToLibrary(book) {
@@ -50,35 +99,63 @@ function addBookToLibrary(book) {
 // Runs a loop on the myLibrary array to see if there's any book. Pass the each array (that is an object)
 // to a nested loop that create elements and append them to the container
 function displayBook(library) {
+            // Create a remove button
+        let removeBtn = document.createElement('button');
+        removeBtn.setAttribute('class', 'remove-content');
+
         let book = library[library.length-1];
-        let bookContainer = document.createElement('div');
+        let bookContainer = document.createElement('span');
         bookContainer.classList.add('book-container');
         for ( detail in book ) {
+            if ( detail == 'id') {
+                    bookContainer.setAttribute('data-key', id);
+                continue;
+            }
+            // creation of elements
             let bookAttribute = document.createElement('span');
             let bookDetail = document.createElement('span');
-            let bookLabel = document.createElement('span');
+            let bookLabel;
+            if ( detail !== 'status' ) {
+                bookLabel = document.createElement('span');
+            } else {
+                bookLabel = document.createElement('button');
+                bookLabel.setAttribute('data-key', id);             
+            }
 
+            // change value of true or false to another string
             if ( book[detail] === 'true' ) {
                 book[detail] = "Read";
                 bookLabel.setAttribute('class', 'read');
             } else if ( book[detail] === 'false' ) {
                 book[detail] = "Not read";
                 bookLabel.setAttribute('class', 'not-read');
-            } 
+            }
 
+            // add class to each created element
             bookAttribute.classList.add(`${detail}`);
 
+            // add labels to each book's detail except for the book's name
             if (!(detail == 'name')) {
                 bookLabel.classList.add('book-label');
                 bookLabel.textContent = `${detail}`;
                 bookAttribute.appendChild(bookLabel);
             }
 
-            bookDetail.classList.add('book-content');
+            // now, add text to the book's detail and append them to show up on the page
+            if (!(detail == 'status')) {
+                bookDetail.classList.add('book-content');
+            } else {
+                bookDetail.classList.add('book-content', `status-${id}`);              
+            }
             bookDetail.textContent = book[detail];
             bookAttribute.appendChild(bookDetail);
 
             bookContainer.appendChild(bookAttribute);
+            
+            if ( detail == 'name') {
+                bookAttribute.appendChild(removeBtn);
+                removeBtn.setAttribute('data-key', id);
+            }
 
         }
         container.appendChild(bookContainer);
@@ -87,7 +164,9 @@ function displayBook(library) {
 }
 
 function runInput(name, author, page, genre, read) {
-    let newBook = new book(name, author, page, genre, read);
+    let newBook = new book(name, author, page, genre, read, id);
     addBookToLibrary(newBook);
     displayBook(myLibrary);
+    removeBtn = document.querySelectorAll('.remove-content');
+    statusBtn = document.querySelectorAll('.status');
 }
